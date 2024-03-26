612e53e3c37a404e081865da5efee57bd56b89cf
roxaneseibel <92251395+roxaneseibel@users.noreply.github.com>
2024-03-26 11:07:50 +0000

    creation login

--- OOP_Final_Project/Controllers/LoginController.cs
+++ OOP_Final_Project/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OOP_Final_Project.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,38 @@ namespace OOP_Final_Project.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Authorise(Account ac)
+        {
+            using (DBModels db = new DBModels())
+            {
+                var userDetail = db.Account.Where(x => x.FisrtName == ac.FisrtName && x.LastName == ac.LastName && x.Password == ac.Password).FirstOrDefault();
+                if (userDetail == null)
+                {
+                    ac.LoginErrorMessage = "Wrong Identification or Passeword";
+                    return View("Index", ac);
+                }
+                else
+                {
+                    Session["id"] = userDetail.Id;
+                    Session["email"] = userDetail.Email;
+                    Session["name"] = userDetail.FisrtName;
+                    Session["lastName"] = userDetail.LastName;
+                    Session["accountType"] = userDetail.AccountType;
+                    return RedirectToAction("Index", "Student", userDetail);
+                }
+            }
+
+        }
+
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
--- OOP_Final_Project/Models/Account.cs
+++ OOP_Final_Project/Models/Account.cs
@@ -11,7 +11,9 @@ namespace OOP_Final_Project.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Account
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -22,9 +24,13 @@ namespace OOP_Final_Project.Models
         }
     
         public int Id { get; set; }
+        [DisplayName("FirstName")]
+        [Required(ErrorMessage = "This Field is Required")]
         public string FisrtName { get; set; }
+        [Required(ErrorMessage = "This Field is Required")]
         public string LastName { get; set; }
         public string Email { get; set; }
+        [Required(ErrorMessage = "This Field is Required")]
         public string Password { get; set; }
         public short AccountType { get; set; }
         public Nullable<int> CohortId { get; set; }
@@ -34,5 +40,7 @@ namespace OOP_Final_Project.Models
         public virtual ICollection<Course> Course { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Result> Result { get; set; }
+
+        public string LoginErrorMessage { get; set; }
     }
 }
